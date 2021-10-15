aa6573f9cd6fb2fb5375a135ada386092009c1d5
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2021-10-15 11:48:01 +0200

    indentation

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -12,9 +12,9 @@ const TodoList: React.FC<TodoListProps> = (props) => {
 	return (
 		<ul>
 			{props.items.map(todo => <li key={todo.id}>
-																	<span>{todo.text}</span>
-																	<button onClick={props.onDeleteTodo.bind(null, todo.id)}>Delete</button>
-																</li>)}
+				<span>{todo.text}</span>
+				<button onClick={props.onDeleteTodo.bind(null, todo.id)}>Delete</button>
+			</li>)}
 		</ul>
 	)
 }
